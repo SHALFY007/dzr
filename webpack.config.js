3022482e305e9e051e47b52fe6f39300574d24ff
vladislav <vl_evlashin@mail.ru>
2022-09-23 22:44:24 +0300

    Сделал пятый этап

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,19 @@ module.exports = {
                 test: /\.(png|jpe?g|gif)$/,
                 use: [
                     {
-                        loader: 'file-loader'
+                        loader: 'img-optimize-loader',
+                        options: {
+                            compress: {
+                                mode: 'high',
+                                webp: true,
+                                gifsicle: true,
+                                disableOnDevelopment: false
+                            }
+                        }
                     }
                 ]
-            }
+            },
+
         ]
     },
     plugins: [
